5e1575b89a95e7b335b125890c7c140166780440
Kacper Witosław <wito77pl@gmail.com>
2019-12-22 18:03:17 +0100

    Add new variable computerMove, randomNumber

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+} else if (randomNumber == '2') {
+    computerMove = 'papier';
+} else if (randomNumber == '3') {
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
