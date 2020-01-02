4e96d96fa86338714f334c5aa9b44ef5c28c8fbe
Kacper Witosław <wito77pl@gmail.com>
2020-01-02 13:20:36 +0100

    Add buttons to game

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
+var argButtonName, buttonRock, buttonPaper, buttonScissors;
+
+buttonRock = document.getElementById('button-rock');
+buttonPaper = document.getElementById('button-paper');
+buttonScissors = document.getElementById('button-scissors');
+
+function buttonClicked(argButtonName) {
+    clearMessages();
+    console.log(argButtonName + ' został kliknięty');
+}
+
 var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber, x;
 
-/**
- * Describe this function...
- */
 function getMoveName(argMoveId) {
     console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
     if (argMoveId == 1) {
@@ -17,9 +25,6 @@ function getMoveName(argMoveId) {
     }
 }
 
-/**
- * Describe this function...
- */
 function displayResult(argPlayerMove, argComputerMove) {
     console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
     if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
@@ -35,12 +40,22 @@ function displayResult(argPlayerMove, argComputerMove) {
     }
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+playerMove = argButtonName;
 console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
 console.log('ruch gracza to: ' + playerMove);
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
+
+buttonRock.addEventListener('click', function () {
+    buttonClicked('Kamień');
+});
+buttonPaper.addEventListener('click', function () {
+    buttonClicked('Papier');
+});
+buttonScissors.addEventListener('click', function () {
+    buttonClicked('Nożyce');
+});
